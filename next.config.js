9d03b31f848adb3387298d8f131c314b0aed3956
adekoyadapo <49321398+adekoyadapo@users.noreply.github.com>
2024-03-26 12:22:32 -0600

    initial: update nextconfig for cloudflare

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: 'edge',
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
     ],
   },
